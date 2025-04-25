d4f504ad898b972e903937ec04ed91065fd28d80
zephyr <858128638@qq.com>
2025-04-25 18:24:15 +0800

    修改

--- models/predict_vote.py
+++ models/predict_vote.py
@@ -7,7 +7,7 @@ import re
 # --- 配置 ---
 INPUT_FILE = '../Sample_Set/ArtS_20250325.json' #自然语料库，可以改成人工的
 OUTPUT_FILE = '../output/predictions_vote.json'
-OLLAMA_MODEL = 'qwen2:1.5b'
+OLLAMA_MODEL = 'qwen2:7b'
 
 # 获取脚本所在的目录
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
